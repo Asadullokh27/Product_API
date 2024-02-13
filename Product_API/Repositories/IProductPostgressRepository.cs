b9228d8ad787955aee5e5b28d4468612840f84bf
Otabek <146438558+OtabekgithubJava@users.noreply.github.com>
2024-02-13 20:31:15 +0500

    Update IProductPostgressRepository.cs

--- Product_API/Repositories/IProductPostgressRepository.cs
+++ Product_API/Repositories/IProductPostgressRepository.cs
@@ -1,17 +1,46 @@
-﻿using Product_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using Product_API.Models;
 
 namespace Product_API.Repositories
 {
     public class ProductPostgressRepository : IProductRepository
     {
+        private readonly string _connectionString;
+
+        public ProductPostgressRepository(IConfiguration configuration)
+        {
+            _connectionString = "Server=localhost;Port=5432;Database=exam;Username=postgres;Password=2712;";
+        }
+
         public Product Add(Product product)
         {
-            throw new NotImplementedException();
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var insertedProduct = connection.QueryFirstOrDefault<Product>(
+                    "INSERT INTO products (Name, Description, PhotoPath) VALUES (@Name, @Description, @PhotoPath) RETURNING *",
+                    new { product.Name, product.Description, product.PhotoPath });
+
+                return insertedProduct;
+            }
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var products = connection.Query<Product>("SELECT * FROM products").AsList();
+
+                return products;
+            }
         }
     }
 }
