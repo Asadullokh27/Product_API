3ba34a71ebe7a6f1eaca9fe3715a2cc5df34b8da
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-14 08:35:23 +0500

    Qales

--- Product_API/Repositories/ProductRepository.cs
+++ Product_API/Repositories/ProductRepository.cs
@@ -1,25 +1,57 @@
-﻿using Product_API.Models;
+using Npgsql;
+using Product_API.Models;
 
 namespace Product_API.Repositories
 {
     public class ProductRepository : IProductRepository
     {
-        private readonly List<Product> _products;
+        private readonly string _connectionString;
 
-        public ProductRepository()
+        public ProductRepository(IConfiguration configuration)
         {
-            _products = new List<Product>();
+            _connectionString = configuration.GetConnectionString("PostgresConnection");
         }
 
         public Product Add(Product product)
         {
-            _products.Add(product);
-            return product;
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                NpgsqlCommand command = new NpgsqlCommand($"INSERT INTO products (Id,Name, Description, PhotoPath) VALUES ({product.Id}, '{product.Name}','{product.Description}','{product.PhotoPath}') RETURNING *", connection);
+
+                command.ExecuteNonQuery();
+
+                return product;
+            }
         }
 
         public List<Product> GetAll()
         {
-            return _products;
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                NpgsqlCommand command = new NpgsqlCommand("select * from Products", connection);
+
+                var datareader = command.ExecuteReader();
+
+                List<Product> products = new List<Product>();
+                while (datareader.Read())
+                {
+                    var product = new Product()
+                    {
+                        Id = Convert.ToInt32(datareader[0]),
+                        Name = Convert.ToString(datareader[1]),
+                        Description = Convert.ToString(datareader[2]),
+                        PhotoPath = Convert.ToString(datareader[3]),
+                    };
+
+                    products.Add(product);
+                }
+
+                return products;
+            }
         }
     }
 }
